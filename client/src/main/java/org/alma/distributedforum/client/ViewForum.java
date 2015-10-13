62bb407801cb2558cd421eaa15eb8418631c14a7
Dralagen <dralagen@dralagen.fr>
2015-10-13 10:24:44 +0200

    close ViewForum don't close all application but just unsubscribe

--- client/src/main/java/org/alma/distributedforum/client/ViewForum.java
+++ client/src/main/java/org/alma/distributedforum/client/ViewForum.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.rmi.RemoteException;
 
 public class ViewForum {
@@ -28,9 +30,17 @@ public class ViewForum {
 		
 		/* definition of the window. */
 		JFrame window = new JFrame(customer.getSubject().getName());
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		window.setSize(800, 500);
 		window.setLocationRelativeTo(null);
+
+		window.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				customer.unsubscribe();
+				super.windowClosing(e);
+			}
+		});
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
